8833ab462a1ef2280e7b2e76ac11da3e6dc0e8dd
balkhati <basel.al_khatib@etud.univ-jfc.fr>
2023-01-23 00:26:59 +0100

    TP SP

---
+++ TP-Spring-master/src/main/java/comptoirs/service/LigneService.java
@@ -0,0 +1,64 @@
+package comptoirs.service;
+
+import comptoirs.dao.CommandeRepository;
+import comptoirs.dao.LigneRepository;
+import comptoirs.dao.ProduitRepository;
+import comptoirs.entity.Ligne;
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.Positive;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Service
+@Validated // Les contraintes de validatipn des méthodes sont vérifiées
+public class LigneService {
+    // La couche "Service" utilise la couche "Accès aux données" pour effectuer les traitements
+    private final CommandeRepository commandeDao;
+    private final LigneRepository ligneDao;
+    private final ProduitRepository produitDao;
+
+    // @Autowired
+    // La couche "Service" utilise la couche "Accès aux données" pour effectuer les traitements
+    public LigneService(CommandeRepository commandeDao, LigneRepository ligneDao, ProduitRepository produitDao) {
+        this.commandeDao = commandeDao;
+        this.ligneDao = ligneDao;
+        this.produitDao = produitDao;
+    }
+
+    /**
+     * <pre>
+     * Service métier : 
+     *     Enregistre une nouvelle ligne de commande pour une commande connue par sa clé,
+     *     Incrémente la quantité totale commandée (Produit.unitesCommandees) avec la quantite à commander
+     * Règles métier :
+     *     - le produit référencé doit exister
+     *     - la commande doit exister
+     *     - la commande ne doit pas être déjà envoyée (le champ 'envoyeele' doit être null)
+     *     - la quantité doit être positive
+     *     - On doit avoir une quantite en stock du produit suffisante
+     * <pre>
+     * 
+     *  @param commandeNum la clé de la commande
+     *  @param produitRef la clé du produit
+     *  @param quantite la quantité commandée (positive)
+     *  @return la ligne de commande créée
+     */
+    @Transactional
+    Ligne ajouterLigne(Integer commandeNum, Integer produitRef, @Positive int quantite) {
+        var produit=produitDao.findById(produitRef).orElseThrow();
+        var commande=commandeDao.findById(commandeNum).orElseThrow();
+        var resteEnStock=produit.getUnitesEnStock();
+        var ligne= new Ligne();
+        if (commande.getEnvoyeele()==null&&quantite>0&& resteEnStock>=quantite){
+            ligne.setCommande(commande);
+            ligne.setProduit(produit);
+            ligne.setQuantite(quantite);
+            ligneDao.save(ligne);
+            produitDao.save(produit);
+            produit.setUnitesCommandees(produit.getUnitesCommandees() + quantite);
+            produitDao.save(produit);
+        }
+        return ligne;
+
+    }
+}
---
+++ TP-Spring-master/src/test/java/comptoirs/service/LigneServiceTest.java
@@ -0,0 +1,89 @@
+package comptoirs.service;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import comptoirs.dao.CommandeRepository;
+import comptoirs.dao.ProduitRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import jakarta.validation.ConstraintViolationException;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@SpringBootTest
+ // Ce test est basé sur le jeu de données dans "test_data.sql"
+class LigneServiceTest {
+    static final int NUMERO_COMMANDE_DEJA_LIVREE = 99999;
+    static final int NUMERO_COMMANDE_PAS_LIVREE  = 99998;
+    static final int NUMERO_COMMANDE_INEXISTANTE  = 99997;
+    static final int REFERENCE_PRODUIT_DISPONIBLE_1 = 93;
+    static final int REFERENCE_PRODUIT_DISPONIBLE_2 = 94;
+    static final int REFERENCE_PRODUIT_DISPONIBLE_3 = 95;
+    static final int REFERENCE_PRODUIT_DISPONIBLE_4 = 96;
+    static final int REFERENCE_PRODUIT_INDISPONIBLE = 97;
+    static final int REFERENCE_PRODUIT_INEXISTANT = 100;
+    static final int UNITES_COMMANDEES_AVANT = 0;
+
+    @Autowired
+    LigneService service;
+    @Autowired
+    ProduitRepository produitDao;
+
+    @Autowired
+    CommandeRepository commandeDao;
+
+    @Test
+    void onPeutAjouterDesLignesSiPasLivre() {
+        var ligne = service.ajouterLigne(NUMERO_COMMANDE_PAS_LIVREE, REFERENCE_PRODUIT_DISPONIBLE_1, 1);
+        assertNotNull(ligne.getId(),
+        "La ligne doit être enregistrée, sa clé générée"); 
+    }
+
+    @Test
+    void laQuantiteEstPositive() {
+        assertThrows(ConstraintViolationException.class, 
+            () -> service.ajouterLigne(NUMERO_COMMANDE_PAS_LIVREE, REFERENCE_PRODUIT_DISPONIBLE_1, 0),
+            "La quantite d'une ligne doit être positive");
+    }
+    @Test
+    public void testAjouterLigneProduitInexistant() {
+        Integer commandeNum = NUMERO_COMMANDE_PAS_LIVREE;
+        Integer produitRef = REFERENCE_PRODUIT_INEXISTANT;
+        int quantite = 1;
+        assertEquals(Optional.empty(), produitDao.findById(produitRef));
+        assertNotNull(commandeDao.findById(commandeNum));
+
+        assertThrows(NoSuchElementException.class, () -> service.ajouterLigne(commandeNum, produitRef, quantite));
+    }
+    @Test
+    public void testAjouterLigneCommandeInexistante() {
+        Integer commandeNum = NUMERO_COMMANDE_INEXISTANTE;
+        Integer produitRef = REFERENCE_PRODUIT_DISPONIBLE_1;
+        int quantite = 1;
+        assertEquals(Optional.empty(), commandeDao.findById(commandeNum));
+        assertNotNull(produitDao.findById(produitRef));
+
+        assertThrows(NoSuchElementException.class, () -> service.ajouterLigne(commandeNum, produitRef, quantite));
+    }
+    @Test
+    public void testAjouterLigneCommandeDejaEnvoyee() {
+        Integer commandeNum = NUMERO_COMMANDE_DEJA_LIVREE;
+        Integer produitRef = REFERENCE_PRODUIT_DISPONIBLE_1;
+        int quantite = 1;
+        assertThrows(IllegalArgumentException.class, () -> service.ajouterLigne(commandeNum, produitRef, quantite));
+    }
+    @Test
+    public void testVerifierStockCommandePourProduit(){
+        Integer commandeNum = NUMERO_COMMANDE_PAS_LIVREE;
+        Integer produitRef = REFERENCE_PRODUIT_DISPONIBLE_1;
+        int quantite = 5;
+        var produit = produitDao.findById(produitRef).orElseThrow();
+        int quantiteCommandee = produit.getUnitesCommandees() ;
+        service.ajouterLigne(commandeNum, produitRef, quantite);
+        assertEquals(quantiteCommandee + quantite, produit.getUnitesCommandees());
+    }
+}
